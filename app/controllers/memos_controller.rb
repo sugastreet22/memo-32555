e876e1e23ed10dac64853bf46ecf5217cfe879f9
sugastreet22 <re22sugar@gmail.com>
2021-01-11 17:55:41 +0900

    モデル・データベースの作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -6,6 +6,7 @@ class MemosController < ApplicationController
   end
 
   def create
-    render plain: params["memos"]["title"] + ":" + params["memos"]["body"]
+    Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
+    redirect_to "/"
   end
 end
